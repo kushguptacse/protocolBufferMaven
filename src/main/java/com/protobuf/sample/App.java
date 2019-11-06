29abf77f305419f52f80cb3631392473ef961fb7
g521885 <Kush.GUPTA@idemia.com>
2019-11-06 16:25:15 +0530

    proto file renamed from simple.proto to person.proto

--- src/main/java/com/protobuf/sample/App.java
+++ src/main/java/com/protobuf/sample/App.java
@@ -6,6 +6,7 @@ package com.protobuf.sample;
  */
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("main started !!!");
+
 	}
 }
